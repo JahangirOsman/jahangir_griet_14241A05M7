5e5b7d5934151677cffd426730f5809d543a4de0
JahangirOsman <31826926+JahangirOsman@users.noreply.github.com>
2017-09-12 12:26:12 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
-Student s=new Student(1232,"osman",'1997-05-31',90.2);
-		
+//Student s=new Student(1232,"osman",'1997-05-31',90.2);
+StudentGroup sg=new StudentGroup(100);
+sg.getStudents();		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
