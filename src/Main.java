5f8fdb215864a1bb1a57d0c7bdc58073dda1cfe2
JahangirOsman <31826926+JahangirOsman@users.noreply.github.com>
2017-09-12 12:12:59 +0530

    My First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+Student s=new Student(1232,"osman",'1997-05-31',90.2);
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
